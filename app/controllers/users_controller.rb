857fb68219807395f724b6c70dc101e121499a65
aidasyaz <aida.syaz24@gmail.com>
2018-09-21 11:52:03 +0800

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,11 @@ class UsersController < Clearance::UsersController
 		email = user_params.delete(:email)
 		password = user_params.delete(:password)
 		first_name = user_params.delete(:first_name)
-		address = user_params.delete(:address)
-
-
+		
 		Clearance.configuration.user_model.new(user_params).tap do |user|
 		  user.email = email
 		  user.password = password
 		  user.first_name = first_name
-		  user.address = address
 		end
 	end	
 
