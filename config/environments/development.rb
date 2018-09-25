4813eb7d4320059973cc514819faa7f4b7e02554
aidasyaz <aida.syaz24@gmail.com>
2018-09-25 17:52:41 +0800

    profile image for user profile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   require 'carrierwave/orm/activerecord'
+  require 'mini_magick'
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
