1f251cdf510055ca4da7add181a3800f7d626a36
aidasyaz <aida.syaz24@gmail.com>
2018-09-27 14:11:48 +0800

    reservations validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,8 @@ class Reservation < ApplicationRecord
 	validates :end_date, presence: true
 
 	# validate that start date is before end date and not before current date reserving
-	# validates :check_dates
+	validate :check_dates
+	validate :check_dates_today
 	before_create :compute_total_price, :compute_total_days_booked
 
 	def compute_total_price
@@ -15,20 +16,40 @@ class Reservation < ApplicationRecord
 	end
 
 	def compute_total_days_booked
-		# includes end date
-		self.total_days = ((self.end_date+1) - self.start_date)
+		self.total_days = (self.end_date - self.start_date)
 	end
 
-	# def check_dates
-	# 	if self.start_date > self.end_date
-	# 		errors.add(:date_error, "Start date must be before end date")
-	# 	end
+	# validated but no error msg displayed
+	def check_dates
+		if self.start_date > self.end_date 
+			errors.add(:date_error, "Start date must be before end date")
+			# p Reservation.errors.messages
+		end
+	end
 
+	# # validated but no error msg displayed
+	def check_dates_today
+		if self.start_date <= Date.today
+			errors.add(:date_error, "start date cannot be before today")
+		end
+	end
+
+	# validate there is no overlap dates chosen -> (27th oct n 27th oct )
+	# validate there is no overlap with other reservations for the list to book 
+	def overlaps?(other)
+		# find reservations based on list id
+		listing_with_reservations = Listing.find(self.listing_id).Reservation
+		self.start_date <= other.end_date && other.start_date <= self.end_date
+	end
+
+
+	# validate that guest number input = at least max guest number on list
+	
+	# def guest_number
+	# 	self.guest <= @listing.self.guest 
 	# end
 
-# 	def check_dates_today
-# 		if self.start_date < date.today
-# 			errors.add(:date_error "start date cannot be before today")
-# 		end
-# 	end
+
+
+	
 end
